20753c2e41590db086160c1ea27be95454eabe5d
lg0leg <lg.oleg1067@gmail.com>
2022-12-10 14:57:39 +0300

    feat: set name

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -3,19 +3,16 @@ import { PORT } from './constants';
 
 import * as serverService from './services/server.service';
 
-
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect('mongodb+srv://suetolog:sueta222@cluster0.kwdbi00.mongodb.net/?retryWrites=true&w=majority');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
-    })
+    });
   } catch (error) {
     console.log(error);
   }
 })();
-
-
 
 process.on('SIGINT', async () => {
   await mongoose.disconnect();
